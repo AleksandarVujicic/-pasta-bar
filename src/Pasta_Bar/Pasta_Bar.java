0144a4a2bfb4cc19fbffb81875f4ea3c5d2b47c8
Aleksandar Vujicic <aleksaandar9933@gmail.com>
2022-01-08 23:20:52 +0100

    findIndex method

--- src/Pasta_Bar/Pasta_Bar.java
+++ src/Pasta_Bar/Pasta_Bar.java
@@ -3,8 +3,24 @@ package Pasta_Bar;
 public class Pasta_Bar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
 
 	}
+	public static int findIndex(String[] ingredients, String ingredient) {
+		int i = 0;
+		int x = -1;
 
+		while (i < ingredients.length) {
+
+			if (ingredient.equals(ingredients[i])) {
+				x = i;
+				return x;
+			} else if (ingredient.equals("Order")) {
+				return 0;
+			}
+			i++;
+		}
+		return x;
+	}
 }
