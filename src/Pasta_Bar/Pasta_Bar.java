91e3c28e875b0de8a0b53437c2ed65cb3b4468d3
Aleksandar Vujicic <aleksaandar9933@gmail.com>
2022-01-08 23:28:35 +0100

    methods: discount, OrderCompleted, isRegular

--- src/Pasta_Bar/Pasta_Bar.java
+++ src/Pasta_Bar/Pasta_Bar.java
@@ -3,10 +3,9 @@ package Pasta_Bar;
 public class Pasta_Bar {
 
 	public static void main(String[] args) {
-		
-		
 
 	}
+
 	public static int findIndex(String[] ingredients, String ingredient) {
 		int i = 0;
 		int x = -1;
@@ -22,5 +21,29 @@ public class Pasta_Bar {
 			i++;
 		}
 		return x;
+	}
+
+	public static boolean nonexistentIngredient(String[] ingredients, String ingredient) {
+		return (findIndex(ingredients, ingredient) == -1);
+	}
+
+	public static double discount(double totalPrice) {
+		double toPay = ((totalPrice / 100) * 90);
+		return toPay;
+	}
+
+	public static boolean OrderCompleted(String ingredient) {
+		return (ingredient.equals("Order"));
+	}
+
+	public static boolean isRegular(String[] regulars, String phone) {
+		boolean isRegular = true;
+		for (int i = 0; i < regulars.length; i++) {
+			if (phone.equals(regulars[i])) {
+				return isRegular;
+			}
+		}
+		isRegular = false;
+		return isRegular;
 	}
 }
